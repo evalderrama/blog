b9323e95d43146c8f6d42ce73931a3ab24c2000d
glvcto <cto-argentina@glverhaltnis.com>
2011-12-06 15:01:38 -0500

    post model + comment model with scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '304567dc7f2538ce7569667089bc4760252dd8d861f16c712f1ced0907c0ac0f84419fc2df6ade2de3df466f1d3138f01d6b9e1f5d0a068fb10c9d5b58ea0ab7'
